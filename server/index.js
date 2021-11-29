9f6fbef0472efbce2954e337724cd0c28aa79fe2
akjha271314 <akjha271314@gmail.com>
2021-11-29 14:26:03 +0530

    Deleted some useless comments

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-//const CONNECTION_URL = 'mongodb+srv://akjha271314:helloworld99@cluster0.wkojm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
 const PORT = process.env.PORT;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
